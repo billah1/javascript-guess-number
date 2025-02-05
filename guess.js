f3d3a78c181324666959f986ad445559f4ea9d6e
billah1 <billah9578@gmail.com>
2025-02-05 12:15:31 +0600

    update

--- guess.js
+++ guess.js
@@ -1,23 +1,19 @@
-
-var numberOfWon =0;
+var numberOfWon = 0;
 var numberOfLost = 0;
 
-for( i = 1; i <5; i++){
-var guessNumber = parseInt(prompt("enter a number from 1 to 5 :"));
+for (let i = 1; i <= 5; i++) {
+    var guessNumber = parseInt(prompt("Enter a number from 1 to 5:"));
 
-var randomNumber = Math.floor(Math.random()*5) + 1;
+    var randomNumber = Math.floor(Math.random() * 5) + 1;
 
-if(guessNumber == randomNumber ){
-    console.log("you have won .");
-    numberOfWon++;
-}else{
-    console.log("you have lost. random number was" + randomNumber );
-    numberOfLost++;
+    if (guessNumber === randomNumber) {
+        console.log("You have won.");
+        numberOfWon++;
+    } else {
+        console.log("You have lost. Random number was " + randomNumber);
+        numberOfLost++;
+    }
 }
-}
 
-
-document.write("Nummber Of Won = " + numberOfWon + "<br>");
-
-
-document.write("Nummber Of Lost = " + numberOfLost + "<br>");
+document.write("Number Of Won = " + numberOfWon + "<br>");
+document.write("Number Of Lost = " + numberOfLost + "<br>");
